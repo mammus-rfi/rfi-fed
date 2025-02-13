3cb106c104529deffc04004450611162dc7a50c6
Casca0 <lucashborzani@gmail.com>
2025-02-13 13:11:13 -0300

    added custom titlebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import D20Logo from "@/assets/D20";
+import D20Logo from '@/assets/D20';
 
 export default function Home() {
   return (
-    <div className="bg-secondary w-screen h-screen">
-      <main className="flex w-full h-full items-center justify-center">
-        <div className="bg-primary w-96 h-96 flex flex-col items-center justify-evenly rounded-3xl text-color-text">
+    <div className='bg-secondary w-screen h-screen'>
+      <main className='flex w-full h-full items-center justify-center'>
+        <div className='bg-primary w-96 h-96 flex flex-col items-center justify-evenly rounded-3xl text-color-text'>
           <D20Logo />
-          <h1 className="font-[family-name:var(--font-kanit)] font-bold text-2xl text-color-text">ROLL FOR INITIATIVE</h1>
-          <button className="bg-button w-72 h-16 rounded-2xl hover:bg-secondary transition-all font-[family-name:var(--font-inter)] font-extrabold">Login</button>
+          <h1 className='font-[family-name:var(--font-kanit)]'>
+            ROLL FOR INITIATIVE
+          </h1>
+          <button className='bg-button w-72 h-16 rounded-2xl hover:bg-secondary transition-all duration-200 font-[family-name:var(--font-inter)] font-extrabold'>
+            Login
+          </button>
         </div>
       </main>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        "primary": "#31465e",
-        "secondary": "#233243",
-        "terciary": "#e0e0e0",
-        "button": "#2a3c50",
-        "success": "#94c25b",
-        "error": "#a3293f",
-        "color-text": "#E8E6D4"
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
